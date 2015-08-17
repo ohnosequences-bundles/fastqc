f5854d96074ff8311430e6fb919a40a9db005bae
Marina Manrique <mmanrique@era7.com>
2015-08-17 19:33:15 +0200

    add install method

--- src/main/scala/fastqc.scala
+++ src/main/scala/fastqc.scala
@@ -1,12 +1,20 @@
 package ohnosequencesbundles.statika
 
 import ohnosequences.statika._, bundles._, instructions._
+import java.io.File
 
 
-case object fastqc extends Bundle() {
+abstract class Fastqc (val version: String) extends Bundle() {
+
+  val usrbin = "/usr/bin/"
+  val fastqcDir = "FastQC"
 
   def install: Results = {
-    // do someting here
+
+    Seq("wget", s"http://s3-eu-west-1.amazonaws.com/resources.ohnosequences.com/fastqc/0${version}/fastqc_v${version}.zip", "-O", s"fastqc_v${version}.zip") ->-
+    Seq("unzip", s"fastqc_v${version}.zip") ->-
+    Seq("chmod", "+x", new File(s"${fastqcDir}/fastqc").getAbsolutePath) ->-
+    Seq("ln", "-s", new File(s"${fastqcDir}/fastqc").getAbsolutePath, s"${usrbin}/fastqc") ->-
     success(s"${bundleName} is installed")
   }
 
