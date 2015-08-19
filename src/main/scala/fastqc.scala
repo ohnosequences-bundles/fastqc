1449378c2b00c01daa127fa7651375afb226b551
Alexey Alekhin <laughedelic@gmail.com>
2015-08-19 18:28:29 +0200

    First try

--- src/main/scala/fastqc.scala
+++ src/main/scala/fastqc.scala
@@ -4,18 +4,24 @@ import ohnosequences.statika._, bundles._, instructions._
 import java.io.File
 
 
-abstract class Fastqc (val version: String) extends Bundle() {
+abstract class Fastqc(val version: String) extends Bundle() {
 
   val usrbin = "/usr/bin/"
   val fastqcDir = "FastQC"
 
-  def install: Results = {
-
-    Seq("wget", s"http://s3-eu-west-1.amazonaws.com/resources.ohnosequences.com/fastqc/0${version}/fastqc_v${version}.zip", "-O", s"fastqc_v${version}.zip") ->-
-    Seq("unzip", s"fastqc_v${version}.zip") ->-
-    Seq("chmod", "+x", new File(s"${fastqcDir}/fastqc").getAbsolutePath) ->-
-    Seq("ln", "-s", new File(s"${fastqcDir}/fastqc").getAbsolutePath, s"${usrbin}/fastqc") ->-
-    success(s"${bundleName} is installed")
+  val instructions: AnyInstructions = {
+    cmd("wget")(
+      s"http://s3-eu-west-1.amazonaws.com/resources.ohnosequences.com/fastqc/0${version}/fastqc_v${version}.zip",
+      "-O", s"fastqc_v${version}.zip"
+    ) -&-
+    cmd("unzip")(s"fastqc_v${version}.zip") -&-
+    cmd("chmod")(
+      "+x", new File(s"${fastqcDir}/fastqc").getAbsolutePath
+    ) -&-
+    cmd("ln")(
+      "-s", new File(s"${fastqcDir}/fastqc").getAbsolutePath, s"${usrbin}/fastqc"
+    ) -&-
+    say(s"${bundleName} is installed")
   }
 
 }
