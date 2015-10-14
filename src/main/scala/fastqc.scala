661ff07fa81a33cf2ef1c2a539c310be8fe3ffbb
Alexey Alekhin <laughedelic@gmail.com>
2015-10-14 20:15:11 +0200

    Release preparations

--- src/main/scala/fastqc.scala
+++ src/main/scala/fastqc.scala
@@ -1,25 +1,28 @@
-package ohnosequencesbundles.statika
+package ohnosequencesBundles.statika
 
 import ohnosequences.statika._, bundles._, instructions._
 import java.io.File
 
 
-abstract class Fastqc(val version: String) extends Bundle() {
+abstract class FastQC(val version: String) extends Bundle() { fastqc =>
 
-  def instructions: AnyInstructions = {
-    val fastqc = "FastQC/fastqc"
-    val fastqcZip = s"fastqc_v${version}.zip"
+  lazy val fastqcZip = s"fastqc_v${version}.zip"
+  lazy val folder = "FastQC"
+  lazy val name = "fastqc"
+  lazy val binary = new File(fastqc.folder, fastqc.name)
 
-    cmd("wget")(
-      s"http://s3-eu-west-1.amazonaws.com/resources.ohnosequences.com/fastqc/${version}/${fastqcZip}",
-      "-O", fastqcZip
-    ) -&-
-    cmd("unzip")(fastqcZip) -&-
-    cmd("chmod")("+x", fastqc) -&-
-    cmd("ln")("-s", new File(fastqc).getCanonicalPath, "/usr/bin/fastqc") -&-
-    say(s"${bundleName} is installed")
-  }
+  lazy val downloadZip = cmd("wget")(
+    s"http://s3-eu-west-1.amazonaws.com/resources.ohnosequences.com/fastqc/${version}/${fastqcZip}"
+  )
 
-  def fastqc(args: String*): CmdInstructions = cmd("fastqc")(args: _*)
+  lazy val unzip = cmd("unzip")(fastqcZip)
+
+  lazy val makeExecutable = cmd("chmod")("+x", fastqc.binary.getCanonicalPath)
+
+  lazy val linkBinary = cmd("ln")("-s", fastqc.binary.getCanonicalPath, s"/usr/bin/${fastqc.name}")
+
+  def instructions: AnyInstructions = downloadZip -&- unzip -&- makeExecutable -&- linkBinary
+
+  def runFastQC(args: String*): CmdInstructions = cmd("fastqc")(args: _*)
 
 }
